59e27339be481c1cf472eeed6e0e4fed631cb36e
Kyle Gaskill <kylegaskill64@yahoo.com>
2016-11-22 02:28:47 -0500

    -Added "Energy" bar Next item to work on is scrolling backgrounds

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,12 +4,10 @@ using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
 
-    private Text healthText;
 
 	// Use this for initialization
 	void Start ()
     {
-        healthText = GameObject.Find("HealthText").GetComponent<Text>();
 	}
 
 	
@@ -17,9 +15,6 @@ public class PlayerManager : MonoBehaviour {
 	void Update ()
     {
         Stats stats = GetComponent<Stats>();
-
-        // Display health count
-        healthText.text = "Health: " + ((stats.currentHealth / stats.maxHealth) * 100).ToString("0");
 	}
 
 

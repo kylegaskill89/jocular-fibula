b2569a44572a0c074e08c11d873745b763713423
Kyle Gaskill <kylegaskill64@yahoo.com>
2016-12-02 01:49:07 -0500

    -Added debug controls

---
+++ Assets/Scripts/DebugManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DebugManager : MonoBehaviour 
+{
+
+    public bool debugMode = false;
+
+    public Text DebugModeText;
+
+    public GameObject stdEnemy;
+
+    // Use this for initialization
+    void Start () 
+	{
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (debugMode)
+        {
+            DebugModeText.text = "Debug Mode: On";
+        }
+        else
+        {
+            DebugModeText.text = "Debug Mode: Off";
+        }
+
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            debugMode = !debugMode;
+        }
+
+        if (debugMode && (Input.GetKeyDown(KeyCode.Alpha1)))
+        {
+            Instantiate(stdEnemy, new Vector3(0, 2.5f, 0), transform.rotation);
+        }
+	}
+
+}

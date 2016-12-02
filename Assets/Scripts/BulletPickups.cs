0b3366f73437f2778ba15b14bdae9c065a783b6c
Kyle Gaskill <kylegaskill64@yahoo.com>
2016-12-02 01:21:38 -0500

    -Added Laser weapon -Added pickup prefab for weapons -Separated logic for bullet control to allow for more flexibility.

---
+++ Assets/Scripts/BulletPickups.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletPickups : MonoBehaviour {
+
+    public string pickupType;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector3(0, (-1f * Time.deltaTime), 0));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {            
+            ShotManager shotManager = other.GetComponent<ShotManager>();
+            shotManager.bulletType = pickupType;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/ShotManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotManager : MonoBehaviour
+{
+
+    public string bulletType = "StandardBullet";
+
+    public GameObject StdBul;
+    public GameObject LasBul;
+
+    public float cooldownTime = 10f;
+    public float cooldownTimeTwo = 10f;
+
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (bulletType == "StandardBullet")
+        {
+            StandardBullet();
+        }
+
+        if (bulletType == "Laser")
+        {
+            LaserShot();
+        }
+
+    }
+
+    void StandardBullet()
+    {
+
+        GameObject BulletOneSpawn = GameObject.Find("BulletOneSpawn");
+        GameObject BulletTwoSpawn = GameObject.Find("BulletTwoSpawn");
+
+        Stats stats = GetComponent<Stats>();
+
+        // Timer increases between attacks
+
+        cooldownTime += Time.deltaTime;
+        cooldownTimeTwo += Time.deltaTime;
+
+
+        // If the time since the last shot is higher than the attack speed and the player is pressing the button to shoot, then the timer is reset and the shot is fired
+
+        if (cooldownTime >= stats.attackSpeed && Input.GetAxis("Fire1") != 0)
+        {
+            Instantiate(StdBul, BulletOneSpawn.transform.position, transform.rotation);
+            cooldownTime = 0;
+        }
+
+        if (cooldownTimeTwo >= stats.attackSpeed && Input.GetAxis("Fire2") != 0)
+        {
+            Instantiate(StdBul, BulletTwoSpawn.transform.position, transform.rotation);
+            cooldownTimeTwo = 0;
+        }
+
+    }
+
+    void LaserShot()
+    {
+        GameObject BulletFrontSpawn = GameObject.Find("BulletFrontSpawn");
+
+        if (Input.GetAxis("Fire1") != 0)
+        {            
+            Instantiate(LasBul, BulletFrontSpawn.transform.position, transform.rotation);            
+        }
+    } 
+
+}
